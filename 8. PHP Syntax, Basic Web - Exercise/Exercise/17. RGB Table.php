a503684cde7b6af29a252e59ab2c7f9e18f83a0d
MartinBG <MartinBG@abv.bg>
2017-07-23 22:29:51 +0300

    8. PHP Syntax, Basic Web - Exercise

---
+++ 8. PHP Syntax, Basic Web - Exercise/Exercise/17. RGB Table.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>First Steps Into PHP</title>
+    <style>
+        table * {
+            border: 1px solid black;
+            width: 50px;
+            height: 50px;
+        }
+    </style>
+</head>
+<body>
+<table>
+    <tr>
+        <td>
+            Red
+        </td>
+        <td>
+            Green
+        </td>
+        <td>
+            Blue
+        </td>
+    </tr>
+    <?php
+    for ($colorIndex = 51; $colorIndex <= 255; $colorIndex += 51) {
+        $red = "rgb($colorIndex, 0, 0)";
+        $green = "rgb(0, $colorIndex, 0)";
+        $blue = "rgb(0, 0, $colorIndex)";
+        echo "<tr>";
+        echo "<td style='background-color: $red'></td>";
+        echo "<td style='background-color: $green'></td>";
+        echo "<td style='background-color: $blue'></td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
+</body>
+</html>
