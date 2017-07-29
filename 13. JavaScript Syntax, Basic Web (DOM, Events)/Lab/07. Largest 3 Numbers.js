0a2a3fe0ddfa4d6bfcce408a89b04128a607132e
MartinBG <MartinBG@abv.bg>
2017-07-29 14:30:00 +0300

    updated .gitignore and folders clean-up

--- 13. JavaScript Syntax, Basic Web (DOM, Events)/Lab/07. Largest 3 Numbers.js
+++
@@ -1,8 +0,0 @@
-function largest3Numbers(arr) {
-    let nums = arr.map(Number);
-    let numsSorted = nums.sort((a, b) => b - a);
-    let count = Math.min(3, arr.length);
-    for (let i = 0; i < count; i++) {
-        console.log(numsSorted[i]);
-    }
-}
